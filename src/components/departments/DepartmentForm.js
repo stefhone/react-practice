41102e60268ab9b159ec832dd46d7a715b961705
Stefan Hone <stefan.hone@live.co.uk>
2022-07-13 18:47:35 +0100

    created component and initial form values

---
+++ src/components/departments/DepartmentForm.js
@@ -0,0 +1,24 @@
+export const DepartmentForm = (params) => {
+    
+    const initialFormValues = {
+        title: "", location: "", employees: ""
+    }
+
+    return(
+        <form>
+            <div>
+                <label>Title</label>
+                <input type="text" name="title" />
+            </div>
+            <div>
+                <label>Location</label>
+                <input type="text" name="location"/>
+            </div>
+            <div>
+                <label>Employees</label>
+                <input type="email" name="employees"/>
+            </div>
+            <input type="submit" value="Add department" />
+        </form>
+    )
+}
